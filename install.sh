8c96862b5f72dbc728521cd02e27fb1d912d7dfa
Nathan Miller <nathan@nathanmiller.wtf>
2022-06-02 10:45:58 -0500

    Added zsh stuff, terminator config, updated installer

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZSH_PLUGIN_DIR="${SCRIPT_DIR}/zsh-plugins/"
 
 DOTFILES="\
     screenrc \
@@ -8,11 +9,19 @@ DOTFILES="\
     vimrc \
     bash_aliases \
     gitconfig \
+    zshrc \
 "
 
-IFS=' '
 for dotfile in $DOTFILES
 do
     echo "Installing .${dotfile}"
     ln -s ${SCRIPT_DIR}/${dotfile} "${HOME}/.${dotfile}"
 done
+
+for plugin in $ZSH_PLUGIN_DIR*
+do
+    echo "Installing ${plugin}"
+    ln -s ${plugin} ~/.oh-my-zsh/custom/plugins/$(basename $plugin)
+done
+
+ln -s ${SCRIPT_DIR}/terminator $HOME/.config/terminator
