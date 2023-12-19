81e558979c5323b0449179f34ade399808ce65b8
Nathan Miller <nathan@nathanmiller.wtf>
2023-12-18 18:41:23 -0600

    Auto backup dotfiles

--- install.sh
+++ install.sh
@@ -12,16 +12,32 @@ DOTFILES="\
     zshrc \
 "
 
+function install_dotfile() {
+    dotfile=$1
+    target_name=$2
+
+    if [[ $(readlink -f $target_name) != $(readlink -f $dotfile) ]]; then
+        echo "Installing .${dotfile}"
+        if [[ -f $target_name ]]; then
+            mv $target_name "backups/${dotfile}"
+        fi
+        ln -s ${SCRIPT_DIR}/${dotfile} $target_name
+    fi
+}
+
+mkdir -p backups
+
 for dotfile in $DOTFILES
 do
-    echo "Installing .${dotfile}"
-    ln -s ${SCRIPT_DIR}/${dotfile} "${HOME}/.${dotfile}"
+    install_dotfile "$dotfile" "${HOME}/.${dotfile}"
 done
 
 for plugin in $ZSH_PLUGIN_DIR*
 do
     echo "Installing ${plugin}"
-    ln -s ${plugin} ~/.oh-my-zsh/custom/plugins/$(basename $plugin)
+    if [[ ! -f $target_linkname ]]; then
+        ln -s ${plugin} ~/.oh-my-zsh/custom/plugins/$(basename $plugin)
+    fi
 done
 
-ln -s ${SCRIPT_DIR}/terminator $HOME/.config/terminator
+install_dotfile "terminator" "$HOME/.config/terminator"
